fe190dc71a79aa6a52d2b8897be795b012304a1b
MirzoidovMirjalol <m.mirzoidov1995@gmail.com>
2026-02-14 02:04:27 +0500

    get_feeding_info endi app.locales dan eksport qilinadi.

--- app/locales/__init__.py
+++ app/locales/__init__.py
@@ -1,5 +1,5 @@
 """Locales module"""
 
-from .loader import get_text, get_user_language
+from .loader import get_text, get_user_language, get_feeding_info
 
-__all__ = ["get_text", "get_user_language"]
+__all__ = ["get_text", "get_user_language", "get_feeding_info"]
