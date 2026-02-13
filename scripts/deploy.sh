533ea72ec796dd9f365642e5ecb8cec7b4304666
MirzoidovMirjalol <m.mirzoidov1995@gmail.com>
2026-02-14 02:08:37 +0500

    add sleep 5 before getting logs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,6 +24,9 @@ docker compose up -d --build
 log "--- docker compose ps ---"
 docker compose ps
 
+log "--- waiting 5s before fetching logs ---"
+sleep 5
+
 log "--- docker compose logs bot --tail=50 ---"
 docker compose logs bot --tail=50
 
