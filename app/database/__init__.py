3aff93dbd9b2004b063ed4bff1da45575f8c7415
MirzoidovMirjalol <m.mirzoidov1995@gmail.com>
2026-02-14 01:59:13 +0500

    db export

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,11 +1,12 @@
 """Database module"""
 
-from .database import get_db, init_db
+from .database import get_db, init_db, close_db
 from .models import Base, User, Pet, VetProfile, Clinic, Reminder, Ad, History
 
 __all__ = [
     "get_db",
     "init_db",
+    "close_db",
     "Base",
     "User",
     "Pet",
